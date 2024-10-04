4f655da25d6460e39b928d19c218b23ccca9048f
Genesis Benedith <gbenedith@arizona.edu>
2024-10-03 23:17:45 -0700

    Update MyLibrary.java

--- libraryProject335/src/MyLibrary.java
+++ libraryProject335/src/MyLibrary.java
@@ -25,16 +25,55 @@ public class MyLibrary {
 			System.out.print("What would you like to do? \n Enter a number from 1-7 for the following commands: (1- search, 2- rate, 3- add a book, 4- add books, 5- read book, 6- get list of books, 7- get suggested book)");
 			int answer = scanner.nextInt();
 			switch (answer) {
-				case 1: 
-					model.Search();
-				case 2: 
-					model.rate();
+				case 1: // Call searchBooks to search library by various search types
+					
+					Book book = model.searchBooks();
+					System.out.println(book.toString()); // Print string format of string details
+				
+				case 2: // Call searchBookByTitle to search library by title
+					
+					book = model.searchBookByTitle();
+					if (book == null) {
+						System.out.println("Would you like to search again? (y/n)");
+						scanner = new Scanner(System.in);
+					    String searchAgain = scanner.nextLine().toLowerCase().trim(); 
+					    if (searchAgain.equals("y")) {
+                            // Call the search again
+                            book = model.searchBookByTitle();
+                        }
+                    }
+
+                    if (book != null) {
+                        model.rateBook(book);  // If a book is found, proceed with rating book
+                    } else {
+                        System.out.println("No book selected. Returning to the main menu.");
+                    }
+                    break;
+				
 				case 3:
 					model.addBook();
 				case 4: 
 					controller.addBooks();
-				case 5: 
-					model.readBook();
+				
+				case 5: // Changes a book's status to read if found
+					
+					book = model.searchBooks();
+					if (book == null) {
+						System.out.println("Would you like to search again? (y/n)");
+						scanner = new Scanner(System.in);
+					    String searchAgain = scanner.nextLine().toLowerCase().trim(); 
+					    if (searchAgain.equals("y")) {
+                            // Call the search again
+                            book = model.searchBookByTitle();
+                        }
+                    }
+
+                    if (book != null) {
+                        model.readBook(book);  // If a book is found, proceed with changing book status to read
+                    } else {
+                        System.out.println("No book selected. Returning to the main menu.");
+                    }
+					
 				case 6:
 					model.getBooks();
 				case 7:
